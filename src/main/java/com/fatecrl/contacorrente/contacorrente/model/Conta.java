15b7a62ce57dd4c596cd4c2a07c2b5e909ae0add
edu9988 <edu_9988@hotmail.com>
2024-09-05 22:15:57 -0300

    Add equals() method to Conta class

--- src/main/java/com/fatecrl/contacorrente/contacorrente/model/Conta.java
+++ src/main/java/com/fatecrl/contacorrente/contacorrente/model/Conta.java
@@ -68,6 +68,17 @@ public class Conta implements Serializable{
     public boolean equals(Object obj){
 	if( this == obj )
 	    return true;
-	return false;
+	if(obj == null )
+	    return false;
+	if(getClass() != obj.getClass())
+	    return false;
+	Conta other = (Conta) obj;
+	if(id == null){
+	    if(other.id != null)
+		return false;
+	}
+	else if( !if.equals(other.id) )
+	    return false;
+	return true;
     }
 }
