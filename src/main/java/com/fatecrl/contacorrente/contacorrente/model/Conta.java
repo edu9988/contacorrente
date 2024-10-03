eeb5c8b6197180317d0ab6b45d3d23ad77741716
edu9988 <edu_9988@hotmail.com>
2024-10-03 15:40:59 -0300

    Minor changes to model, service and .gitignore

--- src/main/java/com/fatecrl/contacorrente/contacorrente/model/Conta.java
+++ src/main/java/com/fatecrl/contacorrente/contacorrente/model/Conta.java
@@ -13,72 +13,72 @@ public class Conta implements Serializable{
     }
 
     public Conta( Long id ){
-	this.id = id;
+        this.id = id;
     }
 
     public Long getId(){
-	return id;
+        return id;
     }
 
     public Integer getAgencia(){
-	return agencia;
+        return agencia;
     }
 
     public String getNumero(){
-	return numero;
+        return numero;
     }
 
     public String getTitular(){
-	return titular;
+        return titular;
     }
 
     public Double getSaldo(){
-	return saldo;
+        return saldo;
     }
 
     public void setId( Long id ){
-	this.id = id;
+        this.id = id;
     }
 
     public void setAgencia( Integer agencia ){
-	this.agencia = agencia;
+        this.agencia = agencia;
     }
 
     public void setNumero( String numero ){
-	this.numero = numero;
+        this.numero = numero;
     }
 
     public void setTitular( String titular ){
-	this.titular = titular;
+        this.titular = titular;
     }
 
     public void setSaldo( Double saldo ){
-	this.saldo = saldo;
+        this.saldo = saldo;
     }
 
     @Override
     public int hashCode(){
-	final int prime = 31;
-	int result = 1;
-	result = prime*result+( (id == null)? 0 : id.hashCode() );
-	return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime*result+( (id == null)? 0 : id.hashCode() );
+        return result;
     }
 
     @Override
     public boolean equals(Object obj){
-	if( this == obj )
-	    return true;
-	if(obj == null )
-	    return false;
-	if(getClass() != obj.getClass())
-	    return false;
-	Conta other = (Conta) obj;
-	if(id == null){
-	    if(other.id != null)
-		return false;
-	}
-	else if( !id.equals(other.id) )
-	    return false;
-	return true;
+        if( this == obj )
+            return true;
+        if(obj == null )
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        Conta other = (Conta) obj;
+        if(id == null){
+            if(other.id != null)
+            return false;
+        }
+        else if( !id.equals(other.id) )
+            return false;
+        return true;
     }
 }
--- src/main/java/com/fatecrl/contacorrente/contacorrente/service/ContaService.java
+++ src/main/java/com/fatecrl/contacorrente/contacorrente/service/ContaService.java
@@ -11,61 +11,61 @@ public class ContaService{
     private static List<Conta> contas = new ArrayList<Conta>();
 
     public ContaService(){
-	contaFake();
+        contaFake();
     }
 
     private void contaFake(){
-	Conta conta = new Conta();
-	conta.setAgencia(1234);
-	conta.setId(1L);
-	conta.setNumero("12345");
-	conta.setSaldo(1000.0);
-	conta.setTitular("Diego Neri");
-	contas.add(conta);
+        Conta conta = new Conta();
+        conta.setAgencia(1234);
+        conta.setId(1L);
+        conta.setNumero("12345");
+        conta.setSaldo(1000.0);
+        conta.setTitular("Diego Neri");
+        contas.add(conta);
     }
 
     public List<Conta> findAll(){
-	return contas;
+        return contas;
     }
 
     public Conta find(Conta conta){
-	return contas.stream()
-	    .filter( c -> c.equals(conta) )
-	    .findFirst().orElse(null);
+    return contas.stream()
+        .filter( c -> c.equals(conta) )
+        .findFirst().orElse(null);
     }
 
     public Conta find(Long id){
-	return find( new Conta(id) );
+        return find( new Conta(id) );
     }
 
     public void create( Conta conta ){
-	Long newId = (long) (contas.size()+1);
-	conta.setId(newId);
-	contas.add( conta );
+        Long newId = (long) (contas.size()+1);
+        conta.setId(newId);
+        contas.add( conta );
     }
 
     public Boolean delete(Long id){
-	Conta _conta = this.find(id);
-	if(_conta != null){
-	    contas.remove( _conta );
-	    return true;
-	}
-	return false;
+        Conta _conta = this.find(id);
+        if(_conta != null){
+            contas.remove( _conta );
+            return true;
+        }
+        return false;
     }
 
     public Boolean update( Conta conta ){
-	Conta _conta = find( conta );
-	if(_conta != null){
-	    if( _conta.getAgencia() != null && conta.getAgencia() > 0 )
-		_conta.setAgencia( conta.getAgencia() );
-	    if( !conta.getNumero().isBlank() )
-		_conta.setNumero( conta.getNumero() );
-	    if( conta.getSaldo() != null && conta.getSaldo() > 0 )
-		_conta.setSaldo( conta.getSaldo() );
-	    if( !conta.getTitular().isBlank() )
-		_conta.setTitular( conta.getTitular() );
-	    return true;
-	}
-	return false;
+        Conta _conta = find( conta );
+        if(_conta != null){
+            if( _conta.getAgencia() != null && conta.getAgencia() > 0 )
+            _conta.setAgencia( conta.getAgencia() );
+            if( !conta.getNumero().isBlank() )
+            _conta.setNumero( conta.getNumero() );
+            if( conta.getSaldo() != null && conta.getSaldo() > 0 )
+            _conta.setSaldo( conta.getSaldo() );
+            if( !conta.getTitular().isBlank() )
+            _conta.setTitular( conta.getTitular() );
+            return true;
+        }
+        return false;
     }
 }
